3f9c15522afa013027906f026d22d1912d6e8dc2
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-02-28 00:05:44 -0500

    Added amplify auth module

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsantgql08c2d2ba": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
